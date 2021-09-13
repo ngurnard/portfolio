556325c4a442cfe5b9678cff6aaed44f1c638dfe
ngurnard <nk.gurnard@gmail.com>
2021-09-13 11:35:55 -0400

    Updates

---
+++ precache-manifest.7eec18d944dcdad45c89cc98d9c4c09f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2141845da317c88399a2d8c21780afd3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf12cdc59dc98dae04a1",
+    "url": "/portfolio/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "dcaa04aa07640a85b70a",
+    "url": "/portfolio/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "bed196335248f721a0eb",
+    "url": "/portfolio/static/css/main.e789bf21.chunk.css"
+  },
+  {
+    "revision": "760e7bf305cc182c5657",
+    "url": "/portfolio/static/js/2.c309ce74.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.c309ce74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf12cdc59dc98dae04a1",
+    "url": "/portfolio/static/js/3.eb3106c7.chunk.js"
+  },
+  {
+    "revision": "dcaa04aa07640a85b70a",
+    "url": "/portfolio/static/js/4.d992b363.chunk.js"
+  },
+  {
+    "revision": "bed196335248f721a0eb",
+    "url": "/portfolio/static/js/main.29b4b30a.chunk.js"
+  },
+  {
+    "revision": "f8492d22e4bd7c609b64",
+    "url": "/portfolio/static/js/runtime-main.f1bb9c13.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e92639fdf4a46d1e38f307e8dc082bc4",
+    "url": "/portfolio/static/media/AutonomousCar.e92639fd.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "97ea22ee67c2f4f7d6504d551298f6d1",
+    "url": "/portfolio/static/media/UCI_seal.97ea22ee.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3dbef955787018fe8514c689c23ff4fc",
+    "url": "/portfolio/static/media/corrplot.3dbef955.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "176b328178d94eb9e285fc5f4233b67d",
+    "url": "/portfolio/static/media/inNout.176b3281.jpeg"
+  },
+  {
+    "revision": "9b2a854bbafa6a05472bac0a163e8908",
+    "url": "/portfolio/static/media/jerseyMikes.9b2a854b.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4496f8556e6e6bb10538c89602956626",
+    "url": "/portfolio/static/media/marsRover.4496f855.png"
+  },
+  {
+    "revision": "f551d9901bd76613bf0b3becb55a4a68",
+    "url": "/portfolio/static/media/pennShield2.f551d990.png"
+  },
+  {
+    "revision": "3502d9435fcbd47aa46e69d8ab8b3323",
+    "url": "/portfolio/static/media/psa.3502d943.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "a1ac9b6a95e01ab7e9a84f9d75ee6cb8",
+    "url": "/portfolio/static/media/quadcopter.a1ac9b6a.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "257becc58e57834564b2d3fb2c437678",
+    "url": "/portfolio/static/media/wearableSensing.257becc5.png"
+  }
+]);
